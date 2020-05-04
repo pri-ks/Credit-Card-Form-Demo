1ec1c0a4ba88078d1522bde172ed0e9334046ee0
pri-ks <sharma.priyanka25007@gmail.com>
2020-05-04 11:20:01 +0530

    Update InputBox.js

--- src/components/UI/InputBox/InputBox.js
+++ src/components/UI/InputBox/InputBox.js
@@ -1,52 +1,77 @@
 import React from 'react';
 
-const inputbox=(props) =>{
-    let inputElement=null
-    switch(props.elementType)
-    {
-        case ('input') :
-            inputElement=<input
-            className='inputElement'
-                {...props.elementConfig}
-                value={props.value.toUpperCase()}
-                onChange={props.changed}
-                onFocus={ props.focus } 
-                />;
-            break;
-        
-        case ('select') :
-                inputElement=(
-                    <select className='inputElement' defaultValue='default' 
-                    onChange={props.changed}
-                    onFocus={ props.focus } 
-                    >
-                        <option value='default' disabled>
-                            {props.elementConfig.default}
-                        </option>
-                        {props.elementConfig.options.map(option => (
-                        <option key={option.value} value={option.value}>
-                            {option.displayValue}
-                        </option>
-                    ))}
-                    </select>
-                );
-            break;
-        default: 
-        inputElement=<input
-        className='inputElement'
-            {...props.elementConfig}
-            value={props.value}
-            onChange={props.changed}
-            onFocus={ props.focus } 
-            />;
-        break;
-    }
 
-    return(
-        <div className='inputWrap'>
-            <label className='inputLabel'>{props.label}</label>
-            {inputElement}
-        </div>
-    )
+const Card=(props)=>{
+    const{cardnum,cardname,exmonth,exyear,cvv,focusedElement} =props
+    let cardArray=[];
+    let temp=[]
+    let cvvnum=cvv.split('');
+        temp=cardnum.split('');
+        console.log(temp)
+            for(let i=0;i<16;i++)
+            {
+                    if(i<=temp.length && temp.length>0 && temp[i] !==undefined)
+                    {
+                        if(i>4 && i<11)
+                        {
+                            cardArray.push('*') 
+                        }
+                        else{
+                            console.log(temp[i])
+                            cardArray.push(temp[i]);
+                        }
+                    }
+                    else{
+                            cardArray.push('#');
+                    }
+            }
+        let logo=''
+        if(cardArray[0]==='5')
+            logo='cardLogo2'
+         else if(cardArray[0]==='3')
+            logo='cardLogo3'
+        else if(cardArray[0]==='6')
+            logo='cardLogo4'
+        else
+            logo='cardLogo1'
+        return(
+            <div className={(focusedElement==='cardcvv') ? 'cardBg cardBgBack' :'cardBg'}>
+                 <div className='card-side cardBack'>
+                    <div className='blackBg'>
+                    </div>
+                    <div className='cvvWrap'>
+                        <div>CVV</div>
+                        <div className='cvvNum'>
+                        {
+                            cvvnum.map((val,index) =>(
+                                <span key= {index}>*</span>
+                            ))
+                        }
+                        </div>
+                    </div>
+                    <div className={`margin20 cardLogo ${logo}`}></div>
+                </div>
+                <div className={(focusedElement==='cardcvv') ? 'card-side cardFront cardFrontActive' :'card-side cardFront'}>
+                    <div className={`cardLogo ${logo}`}></div>
+                    <div className={(focusedElement==='cardnumber') ? 'borderWhite cardNumber' :'cardNumber'}>{
+                        cardArray.map((val,index)=>(
+                        <span key={index}>{val}</span>
+                        ))
+                    }</div> 
+                    <div className='cardDetail'>
+                            <div className={(focusedElement==='cardname') ? 'borderWhite cardHolder' :'cardHolder'}>
+                                <p className='title'>Card Holder</p>
+                                <p className='detailText'>{(cardname==='')?'AD SOYAD' :cardname}</p>
+                            </div>
+                            <div className={(focusedElement==='expirymonth' || focusedElement==='expiryyear') ? 'borderWhite cardExpiry' :'cardExpiry'}>
+                                <p className='title'>Expires</p>
+                                <p className='detailText'>{(exmonth==='')?'MM' :exmonth}/{(exyear==='')? 'YY' : (exyear.toString().slice(-2))}</p>
+                            </div>
+                    </div>
+                </div>
+               
+            </div>
+           
+        )
 }
-export default inputbox
+export default Card
